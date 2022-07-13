a1321784aadc2ef90ce8f9a24eedb2e12028c2c1
Timkoo <bfg20k@mail.ru>
2022-07-13 15:22:53 +0300

    добавлена маршрутизация

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,30 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRouts, AuthorizationStatus } from '../../const';
+import { Favorites } from '../../pages/favorites/favorites';
+import { Login } from '../../pages/login/login';
+import { Main } from '../../pages/main/main';
+import { PageNotFound } from '../../pages/page-not-found/page-not-found';
+import { Property } from '../../pages/property/property';
+import { Layout } from '../layout/layout';
+import { PrivateRoute } from '../private-route/private-route';
 
-export default App;
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRouts.Main} element={<Layout />}>
+        <Route index element={<Main cardsNumber={5} />} />
+        <Route
+          path={AppRouts.Favorites}
+          element={
+            <PrivateRoute authorization={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRouts.Login} element={<Login />} />
+        <Route path={AppRouts.Property} element={<Property />} />
+      </Route>
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
