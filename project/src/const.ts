f5b72cac76622365f016845cbfc346d7d9a75b05
Timkoo <bfg20k@mail.ru>
2022-07-13 15:24:10 +0300

    добавлена маршрутизация 2

---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRouts {
+  Login = 'login',
+  Favorites = 'favorites',
+  Main = '/',
+  Property = 'property',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
