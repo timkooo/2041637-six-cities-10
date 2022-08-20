e695559683d7cb42f1e54214dd2d1dfb872f501e
Timkoo <bfg20k@mail.ru>
2022-08-20 22:13:04 +0300

    добавлены тесты

--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -58,8 +58,8 @@ export const Room = () => {
     return <LoadingScreen />;
   }
 
-  //if (!currentPlace /*|| !params.id*/) {
-  if (!currentPlace || !params.id) {
+  if (!currentPlace /*|| !params.id*/) {
+  //if (!currentPlace || !params.id) {
     return <PageNotFound />;
   }
 
